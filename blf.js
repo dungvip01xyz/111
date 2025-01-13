c77408cc00648c2a25c768c4592aff302f798d9b
dungvip01xyz <168471127+dungvip01xyz@users.noreply.github.com>
2025-01-13 11:40:19 +0700

    Update blf.js

--- blf.js
+++ blf.js
@@ -1,9 +1,9 @@
 zdjl.runActionAsync({
-    type: "按键",
-    delayUnit: 1,
-    keyCode: 184
-  });
+  type: "按键",
+  keyCode:187
+});
 sleep(1000)
 zdjl.click('50%', '6%')
 sleep(1000)
 zdjl.click('50%', '32%')
+
