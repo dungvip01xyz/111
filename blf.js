2911a032e9d7bf5175fd4b1a4a260597da52a139
dungvip01xyz <168471127+dungvip01xyz@users.noreply.github.com>
2025-01-14 19:31:58 +0700

    Update blf.js

--- blf.js
+++ blf.js
@@ -1,3 +1,12 @@
+function inputText(text) {
+  zdjl.runActionAsync({
+      type: "输入内容",
+      delayUnit: 1,
+      inputText: text,
+      clearBeforeInput: true
+  });
+}
+sleep(1000)
 zdjl.runActionAsync({
   type: "按键",
   keyCode:187
@@ -6,4 +15,12 @@ sleep(1000)
 zdjl.click('50%', '6%')
 sleep(1000)
 zdjl.click('50%', '32%')
-
+sleep(1000)
+zdjl.click('33%', '15%')
+sleep(1000)
+zdjl.click('33%', '15%')
+inputText("blox fruit")
+sleep(1000)
+zdjl.click('22%', '20%')
+sleep(1000)
+zdjl.click('30%', '40%')
