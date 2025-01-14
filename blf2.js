4172c225ae3869cc23eb5c15886b6e5da218efd6
dungvip01xyz <168471127+dungvip01xyz@users.noreply.github.com>
2025-01-14 20:57:35 +0700

    Create blf2.js

---
+++ blf2.js
@@ -0,0 +1,45 @@
+var pack = zdjl.readFile('/sdcard/tool/pack.txt');
+var lines = pack.split('\n');
+function inputText(text) {
+    zdjl.runActionAsync({
+        type: "输入内容",
+        delayUnit: 1,
+        inputText: text,
+        clearBeforeInput: true
+    });
+  }
+function key(keyCode) {
+    zdjl.runActionAsync({
+        type: "按键",
+        keyCode: keyCode
+      });
+}
+for (var i = 0; i < lines.length; i++) {
+    zdjl.toast(lines[i]);
+    zdjl.runActionAsync({
+        type: "打开应用",
+        delayUnit: 1,
+        appPackageName: lines[i],
+        closeBeforeOpen: false,
+        onlyClose: false
+    });
+    zdjl.sleep(2000);
+    key(187);
+    sleep(1000)
+    zdjl.click('6%', '50%')
+    sleep(2000)
+    zdjl.click('31%', '50%')
+    sleep(2000)
+    key(4);
+    sleep(1000)
+    zdjl.click('33%', '15%')
+    sleep(1000)
+    zdjl.click('33%', '15%')
+    sleep(1000)
+    inputText("blox fruit")
+    sleep(1000)
+    zdjl.click('22%', '20%')
+    sleep(1000)
+    zdjl.click('30%', '40%')
+
+}
