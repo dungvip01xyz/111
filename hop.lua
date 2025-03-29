8eb091a1fdfd2d454edeade0edd0f2a4e6acda1d
dungvip01xyz <168471127+dungvip01xyz@users.noreply.github.com>
2025-03-29 22:28:55 +0700

    Update hop.lua

--- hop.lua
+++ hop.lua
@@ -1,51 +1,3 @@
-function fixlag()
-    local lighting = game:GetService("Lighting")
-    local g = game
-    local w = g.Workspace
-    local l = g.Lighting
-    local t = w.Terrain
-    if lighting:FindFirstChild("FantasySky") then
-        lighting.FantasySky:Destroy()
-    end
-    t.WaterWaveSize = 0
-    t.WaterWaveSpeed = 0
-    t.WaterReflectance = 0
-    t.WaterTransparency = 0
-    l.GlobalShadows = false
-    l.FogEnd = 9e9
-    l.Brightness = 0
-    for _, v in pairs(g:GetDescendants()) do
-        if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then 
-            v.Material = Enum.Material.Plastic
-            v.Reflectance = 0
-        elseif v:IsA("Decal") or v:IsA("Texture") then
-            v.Transparency = 1
-        elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
-            v.Lifetime = NumberRange.new(0)
-        elseif v:IsA("Explosion") then
-            v.BlastPressure = 1
-            v.BlastRadius = 1
-        elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
-            v.Enabled = false
-        elseif v:IsA("MeshPart") then
-            v.Material = Enum.Material.Plastic
-            v.Reflectance = 0
-            v.TextureID = "rbxassetid://10385902758728957"
-        end
-    end
-    for _, e in pairs(l:GetChildren()) do
-        if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or 
-           e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
-            e.Enabled = false
-        end
-    end
-    for _, v in pairs(game:GetService("Workspace").Camera:GetDescendants()) do
-        if v:IsA("Part") and v.Material == Enum.Material.Water then
-            v.Transparency = 1
-            v.Material = Enum.Material.Plastic
-        end
-    end
-end
 function HopLower()
     local maxplayers, gamelink, goodserver, data_table = math.huge, "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
     if not _G.FailedServerID then _G.FailedServerID = {} end
@@ -60,7 +12,6 @@ function HopLower()
             end)
         end
     end
-
     function getservers()
         pcall(serversearch)
         for i, v in pairs(data_table) do
@@ -75,7 +26,6 @@ function HopLower()
             end
         end
     end
-
     pcall(getservers)
     wait(.1)
     if goodserver == game.JobId or maxplayers == #game:GetService"Players":GetChildren() - 1 then
@@ -106,7 +56,7 @@ function HopLower()
     end
 end
 local Players = game:GetService("Players")
-local localPlayer = Players.LocalPlayer --
+local localPlayer = Players.LocalPlayer -- Lấy người chơi đang chạy script
 local playerNames = getgenv().CheckpPlayer
 getgenv().CheckpPlayers
 for _, playerName in ipairs(playerNames) do 
@@ -117,10 +67,9 @@ for _, playerName in ipairs(playerNames) do
         if player then
             print(playerName .. " đang ở trong server, tôi thoát game.")
             HopLower()
-            break 
+            break -- Dừng vòng lặp ngay khi tìm thấy người cần tránh
         else
             print(playerName .. " không có trong server, tôi ở lại.")
-            fixlag()
         end
     end
 end
