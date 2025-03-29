612620c637747a0c78b45d47ed047e39999aa85f
dungvip01xyz <168471127+dungvip01xyz@users.noreply.github.com>
2025-03-29 21:48:12 +0700

    Create hop.lua

---
+++ hop.lua
@@ -0,0 +1,126 @@
+function fixlag()
+    local lighting = game:GetService("Lighting")
+    local g = game
+    local w = g.Workspace
+    local l = g.Lighting
+    local t = w.Terrain
+    if lighting:FindFirstChild("FantasySky") then
+        lighting.FantasySky:Destroy()
+    end
+    t.WaterWaveSize = 0
+    t.WaterWaveSpeed = 0
+    t.WaterReflectance = 0
+    t.WaterTransparency = 0
+    l.GlobalShadows = false
+    l.FogEnd = 9e9
+    l.Brightness = 0
+    for _, v in pairs(g:GetDescendants()) do
+        if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then 
+            v.Material = Enum.Material.Plastic
+            v.Reflectance = 0
+        elseif v:IsA("Decal") or v:IsA("Texture") then
+            v.Transparency = 1
+        elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+            v.Lifetime = NumberRange.new(0)
+        elseif v:IsA("Explosion") then
+            v.BlastPressure = 1
+            v.BlastRadius = 1
+        elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
+            v.Enabled = false
+        elseif v:IsA("MeshPart") then
+            v.Material = Enum.Material.Plastic
+            v.Reflectance = 0
+            v.TextureID = "rbxassetid://10385902758728957"
+        end
+    end
+    for _, e in pairs(l:GetChildren()) do
+        if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or 
+           e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
+            e.Enabled = false
+        end
+    end
+    for _, v in pairs(game:GetService("Workspace").Camera:GetDescendants()) do
+        if v:IsA("Part") and v.Material == Enum.Material.Water then
+            v.Transparency = 1
+            v.Material = Enum.Material.Plastic
+        end
+    end
+end
+function HopLower()
+    local maxplayers, gamelink, goodserver, data_table = math.huge, "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
+    if not _G.FailedServerID then _G.FailedServerID = {} end
+    local function serversearch()
+        data_table = game:GetService"HttpService":JSONDecode(game:HttpGetAsync(gamelink))
+        for _, v in pairs(data_table.data) do
+            pcall(function()
+                if type(v) == "table" and v.id and v.playing and tonumber(maxplayers) > tonumber(v.playing) and not table.find(_G.FailedServerID, v.id) then
+                    maxplayers = v.playing
+                    goodserver = v.id
+                end
+            end)
+        end
+    end
+
+    function getservers()
+        pcall(serversearch)
+        for i, v in pairs(data_table) do
+            if i == "nextPageCursor" then
+                if gamelink:find"&cursor=" then
+                    local a = gamelink:find"&cursor="
+                    local b = gamelink:sub(a)
+                    gamelink = gamelink:gsub(b, "")
+                end
+                gamelink = gamelink .. "&cursor=" .. v
+                pcall(getservers)
+            end
+        end
+    end
+
+    pcall(getservers)
+    wait(.1)
+    if goodserver == game.JobId or maxplayers == #game:GetService"Players":GetChildren() - 1 then
+    end
+    table.insert(_G.FailedServerID, goodserver)
+    game:GetService"TeleportService":TeleportToPlaceInstance(game.PlaceId, goodserver)
+
+    while wait(.1) do
+        pcall(function()
+            if not game:IsLoaded() then
+                game.Loaded:Wait()
+            end
+            game.CoreGui.RobloxPromptGui.promptOverlay.DescendantAdded:Connect(function()
+                local GUI = game.CoreGui.RobloxPromptGui.promptOverlay:FindFirstChild("ErrorPrompt")
+                if GUI then
+                    if GUI.TitleFrame.ErrorTitle.Text == "Disconnected" then
+                        if #game.Players:GetPlayers() <= 1 then
+                            game.Players.LocalPlayer:Kick("\nRejoining...")
+                            wait(.1)
+                            game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+                        else
+                            game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
+                        end
+                    end
+                end
+            end)
+        end)
+    end
+end
+local Players = game:GetService("Players")
+local localPlayer = Players.LocalPlayer --
+local playerNames = getgenv().CheckpPlayer
+getgenv().CheckpPlayers
+for _, playerName in ipairs(playerNames) do 
+    if playerName == localPlayer.Name then
+        print("Tên tôi có trong danh sách, bỏ qua kiểm tra.") 
+    else
+        local player = Players:FindFirstChild(playerName)
+        if player then
+            print(playerName .. " đang ở trong server, tôi thoát game.")
+            HopLower()
+            break 
+        else
+            print(playerName .. " không có trong server, tôi ở lại.")
+            fixlag()
+        end
+    end
+end
